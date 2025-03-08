9d69aa1cbefdac555de861ecdb1facaa530a51a2
SamyuMT <jemycraft12@hotmail.com>
2025-03-08 15:29:16 -0500

    first commit

---
+++ include/validators.py
@@ -0,0 +1,16 @@
+from flask import request
+
+def checkArgs(required_args, request_args):
+    for arg in required_args:
+        if arg in request_args:
+            pass
+        else:
+            raise Exception(f"Falta el parámetro requerido: {arg}")
+
+def parsedRespond(data):
+    temp = {
+        'data' : data,
+        'status': True,
+        'message': 'ok'
+    }
+    return temp
